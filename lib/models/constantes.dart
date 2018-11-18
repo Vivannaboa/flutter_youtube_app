739a56a22e8f50ffd1c1cba26db8df27e6f1b686
Marivaldo Vivan <m.vivan1@gmail.com>
2018-11-18 13:04:30 -0200

    final

--- lib/models/constantes.dart
+++ lib/models/constantes.dart
@@ -1,4 +1,4 @@
-const String apiKey = "AIzaSyBuQuRl0xPxOizHTOt2t1N-eWWNbw8H9-E";
+const String apiKey = "";
 const String channelId = "UC_x5XG1OV2P6uZZ5FSM9Ttw";
 const String channelUrl = "https://www.googleapis.com/youtube/v3/search?part=id&maxResults=20&order=date&channelId="+ channelId +"&key=" + apiKey ;
 const String detailsUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&key=" + apiKey + "&id=";
